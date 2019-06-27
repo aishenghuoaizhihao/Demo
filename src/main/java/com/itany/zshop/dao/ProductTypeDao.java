a5738f0598cc9af3e8fcad9f479e9b8962fa79d9
Your Name <you@example.com>
2019-06-27 16:31:26 +0800

    first commit

---
+++ src/main/java/com/itany/zshop/dao/ProductTypeDao.java
@@ -0,0 +1,10 @@
+package com.itany.zshop.dao;
+
+import com.itany.zshop.pojo.ProductType;
+
+import java.util.List;
+
+public interface ProductTypeDao {
+
+    public List<ProductType> selectAll();
+}
